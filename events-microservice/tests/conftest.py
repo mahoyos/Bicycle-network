9f4421e2256ec8f758d48fff0390ee36b8d154bb
esteban.muriel.roldan.contractor <esteban.muriel.roldan.contractor@bbva.com>
2026-03-26 18:55:58 -0500

    fixed tests

--- events-microservice/tests/conftest.py
+++ events-microservice/tests/conftest.py
@@ -1,23 +1,27 @@
-from app.database import create_tables
-from app import main, database, models
 import os
 import sys
-
-import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 # Add project root to PYTHONPATH so `app` package can be imported from tests.
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
 
+from sqlalchemy.pool import StaticPool
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from fastapi.testclient import TestClient
+import pytest
+from app import main, database, models
+from app.database import create_tables
+
 
 @pytest.fixture(scope="session")
 def test_db_engine():
-    engine = create_engine("sqlite:///:memory:",
-                           connect_args={"check_same_thread": False})
+    engine = create_engine(
+        "sqlite:///:memory:",
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
     TestingSessionLocal = sessionmaker(
         autocommit=False, autoflush=False, bind=engine)
     database.engine = engine
